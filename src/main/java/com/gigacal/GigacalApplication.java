72fa9f5765242bec1bf121c19bd479f12fe21edc
kamil100900 <56301469+nejfik@users.noreply.github.com>
2023-10-17 21:15:27 +0200

    initial commit

---
+++ src/main/java/com/gigacal/GigacalApplication.java
@@ -0,0 +1,13 @@
+package com.gigacal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigacalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigacalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gigacal/GigacalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigacal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigacalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
